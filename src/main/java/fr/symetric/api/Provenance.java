441950c5a4ec4bd5d9806a0be2f63e6027bf7819
Alban Gaignard <aban.gaignard@univ-nantes.fr>
2016-10-28 17:18:42 +0200

    improved performance

--- src/main/java/fr/symetric/api/Provenance.java
+++ src/main/java/fr/symetric/api/Provenance.java
@@ -8,6 +8,7 @@ package fr.symetric.api;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import fr.cnrs.ga2prov.GHistAPI;
+import fr.cnrs.ga2prov.GHistAPI_v2;
 import fr.cnrs.ga2prov.GHistFactory;
 import fr.cnrs.ga2prov.Util;
 import fr.inria.edelweiss.kgram.core.Mappings;
@@ -18,12 +19,15 @@ import fr.inria.edelweiss.kgtool.load.Load;
 import fr.inria.edelweiss.kgtool.print.JSOND3Format;
 import fr.inria.edelweiss.kgtool.print.JSONFormat;
 import fr.symetric.data.GalaxyCredential;
+import fr.symetric.server.models.DAOFactory;
+import fr.symetric.server.models.ProvMetricsRepositoryDAO;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.util.Map;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -43,16 +47,114 @@ public class Provenance {
     private Logger logger = Logger.getLogger(Provenance.class);
     private String headerAccept = "Access-Control-Allow-Origin";
 
+    @GET
+    @Path("/usage")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUsage() {
+
+        StringBuilder response = new StringBuilder();
+        
+        ProvMetricsRepositoryDAO dao = DAOFactory.getProvMetricsDAO();
+        String xAxis = "[";
+        String y1 = "[";
+        String y2 = "[";
+        for (String id : dao.findIds()) {
+            xAxis += "'"+id+"', ";
+            y1 += dao.get(id).getNbProvTriples()+", ";
+            y2 += dao.get(id).getNbProvGen()+", ";
+        }
+        xAxis = xAxis.substring(0, xAxis.lastIndexOf(","));
+        y1 = y1.substring(0, y1.lastIndexOf(","));
+        y2 = y2.substring(0, y2.lastIndexOf(","));
+        xAxis += "]";
+        y1 += "]";
+        y2 += "]";
+        
+        String jsonTpl = "{\n"
+                + "        chart: {\n"
+                + "            zoomType: 'xy'\n"
+                + "        },\n"
+                + "        title: {\n"
+                + "            text: 'Galaxy PROV usage statistics'\n"
+                + "        },\n"
+                + "        xAxis: [{\n"
+                + "            categories: "+xAxis+",\n"
+                + "            crosshair: true\n"
+                + "        }],\n"
+                + "        yAxis: [{ // Primary yAxis\n"
+                + "            labels: {\n"
+                + "                format: '{value}',\n"
+                + "                style: {\n"
+                + "                    color: Highcharts.getOptions().colors[1]\n"
+                + "                }\n"
+                + "            },\n"
+                + "            title: {\n"
+                + "                text: 'PROV generations',\n"
+                + "                style: {\n"
+                + "                    color: Highcharts.getOptions().colors[1]\n"
+                + "                }\n"
+                + "            }\n"
+                + "        }, { // Secondary yAxis\n"
+                + "            title: {\n"
+                + "                text: 'PROV triples generated',\n"
+                + "                style: {\n"
+                + "                    color: Highcharts.getOptions().colors[0]\n"
+                + "                }\n"
+                + "            },\n"
+                + "            labels: {\n"
+                + "                format: '{value} triples',\n"
+                + "                style: {\n"
+                + "                    color: Highcharts.getOptions().colors[0]\n"
+                + "                }\n"
+                + "            },\n"
+                + "            opposite: true\n"
+                + "        }],\n"
+                + "        tooltip: {\n"
+                + "            shared: true\n"
+                + "        },\n"
+                + "        legend: {\n"
+                + "            layout: 'vertical',\n"
+                + "            align: 'left',\n"
+                + "            x: 120,\n"
+                + "            verticalAlign: 'top',\n"
+                + "            y: 100,\n"
+                + "            floating: true,\n"
+                + "            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'\n"
+                + "        },\n"
+                + "        series: [{\n"
+                + "            name: 'PROV triples',\n"
+                + "            type: 'spline',\n"
+                + "            yAxis: 1,\n"
+                + "            data: "+y1+",\n"
+                + "            tooltip: {\n"
+                + "                valueSuffix: ' triples'\n"
+                + "            }\n"
+                + "\n"
+                + "        }, {\n"
+                + "            name: 'Runs',\n"
+                + "            type: 'column',\n"
+                + "            data: "+y2+",\n"
+                + "            tooltip: {\n"
+                + "                valueSuffix: ' runs'\n"
+                + "            }\n"
+                + "        }]\n"
+                + "    }";
+        
+        response.append(jsonTpl);
+
+        return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(response).build();
+    }
+
     @POST
     @Path("/histories")
     @Produces(MediaType.APPLICATION_JSON)
     public Response listHistories(GalaxyCredential cred) {
-        GHistFactory.initInstance(cred.getInstanceUrl(), cred.getApiKey());
+        GHistFactory.init(cred.getInstanceUrl(), cred.getApiKey());
         try {
             StringBuilder response = new StringBuilder();
             response.append("{ \"histories\" : [");
 
-            GHistAPI gAPI = GHistFactory.getInstance();
+            GHistAPI_v2 gAPI = GHistFactory.getInstance();
             Map<String, String> histories = gAPI.listHistories();
             for (String id : histories.keySet()) {
                 response.append("{ \"id\":\"" + id + "\", \"label\":\"" + histories.get(id) + "\"},");
@@ -75,15 +177,23 @@ public class Provenance {
     @Path("/genProv/{hid}")
     @Produces(MediaType.TEXT_PLAIN)
     public Response genProv(GalaxyCredential cred, @PathParam("hid") String hid) {
-        GHistFactory.initInstance(cred.getInstanceUrl(), cred.getApiKey());
+        GHistFactory.init(cred.getInstanceUrl(), cred.getApiKey());
         try {
-            GHistAPI gAPI = GHistFactory.getInstance();
+            GHistAPI_v2 gAPI = GHistFactory.getInstance();
 
             StopWatch sw = new StopWatch();
             sw.start();
             String provTTL = gAPI.getProv(hid);
             sw.stop();
             logger.debug("PROV triples retrieved in " + sw.getTime() + " ms");
+
+            Graph graph = Graph.create(false);
+            Load ld = Load.create(graph);
+            InputStream inputStream = new ByteArrayInputStream(provTTL.getBytes(StandardCharsets.UTF_8));
+            ld.load(inputStream, ".ttl");
+            logger.info("Loaded " + graph.size() + " rdf statements.");
+            ProvMetricsRepositoryDAO dao = DAOFactory.getProvMetricsDAO();
+            dao.traceInsertedTriples(graph.size(), dao.getTimeSlot());
 
 //            StringBuilder response = new StringBuilder();
 //            response.append("{ \"prov_triples_ttl\" : \""+provTTL+"\"}");
@@ -101,9 +211,9 @@ public class Provenance {
     @Produces(MediaType.APPLICATION_JSON)
     public Response visProv(GalaxyCredential cred, @PathParam("hid") String hid) {
         //TODO remove mappings from JSON sended, only keep D3 graph
-        GHistFactory.initInstance(cred.getInstanceUrl(), cred.getApiKey());
+        GHistFactory.init(cred.getInstanceUrl(), cred.getApiKey());
         try {
-            GHistAPI gAPI = GHistFactory.getInstance();
+            GHistAPI_v2 gAPI = GHistFactory.getInstance();
 
             StopWatch sw = new StopWatch();
             sw.start();
@@ -113,20 +223,23 @@ public class Provenance {
 
             sw.reset();
             sw.start();
-            GraphStore graph = GraphStore.create(false);
+//            GraphStore graph = GraphStore.create(false);
+            Graph graph = Graph.create(false);
             QueryProcess exec = QueryProcess.create(graph);
             Load ld = Load.create(graph);
             InputStream inputStream = new ByteArrayInputStream(provTTL.getBytes(StandardCharsets.UTF_8));
             ld.load(inputStream, ".ttl");
             logger.info("Loaded " + graph.size() + " rdf statements.");
+            ProvMetricsRepositoryDAO dao = DAOFactory.getProvMetricsDAO();
+            dao.traceInsertedTriples(graph.size(), dao.getTimeSlot());
 
             Mappings maps = null;
 
             String filterQuery = "PREFIX prov:<http://www.w3.org/ns/prov#>\n"
                     + "CONSTRUCT {\n"
                     + "	?xL ?p ?yL .\n"
-//                    + " ?x rdfs:label ?xL .\n"
-//                    + " ?y rdfs:label ?yL .\n"
+                    //                    + " ?x rdfs:label ?xL .\n"
+                    //                    + " ?y rdfs:label ?yL .\n"
                     + "} WHERE {\n"
                     + "	?x ?p ?y .\n"
                     + " OPTIONAL {?x rdfs:label ?xL} .\n"
@@ -146,6 +259,10 @@ public class Provenance {
                     + JSOND3Format.create(g).toString()
                     + " }";
 
+//            String mapsProvJson = "{ \"mappings\" : "
+//                    + "\"d3\" : "
+//                    + JSOND3Format.create(g).toString()
+//                    + " }";
 //            Gson gson = new Gson();
 //            try {
 //                Object o = gson.fromJson(mapsProvJson, Object.class);
@@ -153,7 +270,6 @@ public class Provenance {
 //            } catch (Exception e) {
 //                logger.error("invalid json format");
 //            }
-
             sw.stop();
             logger.info("Filtered PROV graph and D3 data formatting in " + sw.getTime() + " ms");
 
@@ -167,7 +283,6 @@ public class Provenance {
 //            Files.write(pathProv, provTTL.getBytes(), StandardOpenOption.WRITE);
 //            logger.info("RDF provenance written in " + pathProv.toString());
             /////////////////////////////////////////////         
-
             return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(mapsProvJson).build();
         } catch (Exception ex) {
             logger.error("An error occured while connecting to the Galaxy server. Please check the Galaxy server URL and your API key.");
