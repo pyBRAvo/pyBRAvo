a831c88e5e5323d2d05ee2fd02e1bf73a8651d10
Marie Lefebvre <lefebvre.m2@gmail.com>
2018-01-16 15:17:01 +0100

    Refactor : rooter for regulatory, signaling and batch app

--- src/main/resources/web/js/views/bravoSignalingView.js
+++ src/main/resources/web/js/views/bravoSignalingView.js
@@ -1,15 +1,16 @@
 /** 
- * Systemic view code
+ * BRAvo View codes
+ * For Signaling assembly
  *
  * @author : Marie Lefebvre
  */
 
 var SignalingView = Backbone.View.extend({
-//    el: "#mainContainer", //Container div inside which we would be dynamically loading the templates
+
     initialize: function () {
-//        _.bindAll(this, "render");
+
         this.render();
-        console.log('DemoSysBio View Initialized');
+        console.log('Signaling View Initialized');
     },
     render: function () {
         var that = this;
@@ -23,92 +24,9 @@ var SignalingView = Backbone.View.extend({
         return this;
     },
     events: {
-        "click #btnSearchRegulatoryNetwork": "querySearchNetwork",
-        "click #btnRegulatoryNetwork": "renderRegulatoryNetwork",
-        "click #btnSignalingNetwork": "renderSignalingNetwork",
         "click #btnSearchSignalingNetwork": "querySearchSignalingNetwork",
-        "click #btnBatchNetwork": "renderBatchNetwork",
-        "click #btnRunBatchUp": "runBatchUp",
-        "click #query-type": "queryType",
         "click #btnTFs": "queryTFs"
         
-    },
-    renderRegulatoryNetwork: function () {
-        var that = this;
-        //Fetching the template contents
-        $.get('templates/demo-systemic-regulatory.html', function (data) {
-            template = _.template(data, {});//Option to pass any dynamic values to template
-            that.$el.html(template());//adding the template content to the main template.
-            
-        }, 'html');
-        return this;
-    },
-    renderSignalingNetwork: function () {
-        var that = this;
-        //Fetching the template contents
-        $.get('templates/demo-systemic-signaling.html', function (data) {
-            template = _.template(data, {});//Option to pass any dynamic values to template
-            that.$el.html(template());//adding the template content to the main template.
-            
-        }, 'html');
-        return this;
-    },
-    renderBatchNetwork: function () {
-        var that = this;
-        //Fetching the template contents
-        $.get('templates/demo-systemic-batch.html', function (data) {
-            template = _.template(data, {});//Option to pass any dynamic values to template
-            that.$el.html(template());//adding the template content to the main template.
-            
-        }, 'html');
-        return this;
-    },
-    querySearchNetwork: function () {
-        console.log("querySearchNetworkEvt");
-        var queryType = $('input[name=query-type]:checked').val();
-        // Initialize graphe visualization
-        // !!! var cy is a global variable !!!
-        if(typeof cy === 'undefined'){
-            cy = initialCy();
-        }else {
-            console.log("reinitialize");
-            cy = null;
-            cy = initialCy();
-        }
-        //var cy = initialCy();
-        var genesList = $('#inputGeneList').val().replace(/\s/g, '');
-        if (genesList !== "") {
-            /** 
-             * Hide / display messages
-             */
-            document.getElementById("emptyQuery").style.display = 'none';
-            document.getElementById("errorQuery").style.display = 'none';
-            document.getElementById("noResult").style.display = 'none';
-            document.getElementById("sendingQuery").style.display = 'block';
-            document.getElementById("next-level-regulation").style.display = 'none';
-            /**
-             * Initialize checkbox content
-             */
-            var container = document.getElementById("input-next-regulation");
-            while (container.hasChildNodes()){
-                container.removeChild(container.firstChild);
-            }
-            // Make SPARQL initial query to PathwayCommons endpoint
-            sparqlSysBio(genesList, queryType, cy);
-            // Listen to event
-            $( "#btnRunNextRegulation" ).click(function() {
-                // Get gene list
-                var regulatorList = updateList("regulation");
-                if (regulatorList.length > 0){
-                    // Make SPARQL queries to PathwayCommons endpoint
-                    nextLevelRegulation(regulatorList, cy);
-                }
-            });
-        }else{
-            document.getElementById("emptyQuery").style.display = 'block';
-            document.getElementById("errorQuery").style.display = 'none';
-            document.getElementById("noResult").style.display = 'none';
-        }
     },
     querySearchSignalingNetwork: function () {
         console.log("querySearchSignalingNetworkEvt");
@@ -160,30 +78,7 @@ var SignalingView = Backbone.View.extend({
             document.getElementById("errorQuery").style.display = 'none';
             document.getElementById("noResult").style.display = 'none';
         }
-    },       
-    /**
-    * API to run batch command for upstream
-    */
-    runBatchUp: function() {
-        var genesList = $('#geneList').val().replace(/\s/g, '');
-        if (genesList !== "") {
-            var queryType = $('input[name=input-type]:checked').val();
-            upstreamJob(genesList, queryType);
-        }
-    },        
-    /**
-    * Listen to input type
-    */
-    queryType: function() {
-
-        // When 'id' checked, return 'name'
-        if ($('input[name=query-type]:checked').val() !== 'id'){
-            $('#inputGeneList').val('137964,HGNC:4135,HGNC:2859,P27708');
-        }else {
-            $('#inputGeneList').val('AGPAT6,GALT,DHCR24,FTFD1,CAD');
-        }
-        
-    },        
+    },      
     /**
     * Listen to button 
     * Only TFs for first level of signaling
@@ -200,57 +95,3 @@ var SignalingView = Backbone.View.extend({
         
     }
 });
-
-//var myDemoSysbioView = new DemoSysbioView();
-
-function initialCy() {
-    var initial = cytoscape({
-        container: document.getElementById('cy'), // container to render in
-        //boxSelectionEnabled: false,
-        //autounselectify: true,
-        style: [ // the stylesheet for the graph
-            {
-                selector: 'node',
-                style: {
-                    'background-color': '#898484', // grey
-                    'width' : 12,
-                    'height' : 12,
-                    'label': 'data(id)'
-                }
-            },
-            {
-                selector: '$node > node', // parent - meta node
-                css: {
-                    'padding-top': '10px',
-                    'padding-left': '10px',
-                    'padding-bottom': '10px',
-                    'padding-right': '10px',
-                    'text-valign': 'top',
-                    'text-halign': 'center',
-                    'background-color': '#bbb',
-                    'background-opacity': 0.3
-                }
-            },
-            {
-                selector: 'edge',
-                style: {
-                    'width': 2,
-                    'line-color': '#ccc',
-                    'mid-target-arrow-color': '#ccc',
-                    'mid-target-arrow-shape': 'triangle',
-                    'curve-style': 'bezier'
-                }
-            }
-        ],
-        layout: {
-            name: 'cola',
-            nodeSpacing: 5,
-            avoidOverlap: true,
-            maxSimulationTime: 4000,
-            flow: { axis: 'y'},
-            unconstrIter: 10,
-            handleDisconnected: true
-        }
-    });
-    return initial;
-}
