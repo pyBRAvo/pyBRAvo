5340b75e43f72ff1832d80fb533aa6d5926b4080
Jeremie BOURDON <jeremie.bourdon@univ-nantes.fr>
2019-07-12 16:19:22 +0200

    Update python/bravo/util.py, python/bravo/signaling.py files

--- python/bravo/util.py
+++ python/bravo/util.py
@@ -209,7 +209,7 @@ def gen_small_mol_filter(skip_small_molecules = True, networktype="regulation"):
         if networktype=="regulation":
             return 'FILTER( str(?controllerType) != "http://www.biopax.org/release/biopax-level3.owl#SmallMolecule" and str(?controlledType) != "http://www.biopax.org/release/biopax-level3.owl#SmallMolecule")  .'
         else:
-            return 'FILTER( str(?controllerType) != "http://www.biopax.org/release/biopax-level3.owl#SmallMolecule" and str(?right) != "http://www.biopax.org/release/biopax-level3.owl#SmallMolecule")  .'
+            return 'FILTER( str(?controllerType) != "http://www.biopax.org/release/biopax-level3.owl#SmallMolecule" and str(?rightType) != "http://www.biopax.org/release/biopax-level3.owl#SmallMolecule")  .'
 
     else:
         return ''
