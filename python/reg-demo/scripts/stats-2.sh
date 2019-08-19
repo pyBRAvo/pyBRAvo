881a0d72cd31e42f35a88a287971975bdb060686
Maxime Folschette <Maxime.Folschette@ls2n.fr>
2019-08-19 13:42:22 +0200

    update regulation notebook for final stats

--- python/reg-demo/scripts/stats-2.sh
+++ python/reg-demo/scripts/stats-2.sh
@@ -2,71 +2,47 @@
 
 # Print stats on a BRAvo results' folder
 
-FOLDER=$1
+NAME=$1
+shift
+STDOUT=$1
 shift
 
-if [ -z "$FOLDER" ]
+if [ -z "$NAME" ] || [ -z "$STDOUT" ]
 then
-  echo 'Usage: bash stats-2.sh <folder>'
+  echo 'Usage: bash stats-2.sh <base_name> <stdout_file>'
   exit 1
 fi
 
-echo "   ******"
-echo $FOLDER
-echo "   ******"
-cd $FOLDER
-
-# Base (no unification)
-echo 'Without unification:'
-echo '--------------------'
 # Graph size
 echo -n 'Nodes: '
-wc -l out.sif.genes
+wc -l "${NAME}.genes"
 echo -n 'Edges: '
-wc -l out.sif
+wc -l "${NAME}"
 echo -n ' - signed: '
-grep -cv 'UNKNOWN\|PART_OF' out.sif
+grep -cv 'UNKNOWN\|PART_OF' "${NAME}"
 echo -n ' - unsigned: '
-grep -c 'UNKNOWN' out.sif
+grep -c 'UNKNOWN' "${NAME}"
 echo -n ' - PART_OF: '
-grep -c 'PART_OF' out.sif
-echo -n 'Coverage: '
-python3 ../../compute_coverage.py out.sif ../../input-910.csv ../../Homo_sapiens.gene_info
+grep -c 'PART_OF' "${NAME}"
 
 echo ''
 
-# Unified
-echo 'With unification:'
-echo '-----------------'
-# Graph size
-echo -n 'Nodes: '
-wc -l out-unified.sif.genes
-echo -n 'Edges: '
-wc -l out-unified.sif
-echo -n ' - signed: '
-grep -cv 'UNKNOWN\|PART_OF' out-unified.sif
-echo -n ' - unsigned: '
-grep -c 'UNKNOWN' out-unified.sif
-echo -n ' - PART_OF: '
-grep -c 'PART_OF' out-unified.sif
 echo -n 'Coverage: '
-python3 ../../compute_coverage.py out-unified.sif ../../input-910.csv ../../Homo_sapiens.gene_info
+python3 ../scripts/compute_coverage.py "${NAME}" ../data/input-910.csv ../../Homo_sapiens.gene_info
 
 echo ''
 
 # Complexes
-COMPLEXES=$(grep ' decomposed into' std.out | awk -F' decomposed into' '{print $1}' | sort -u)
+COMPLEXES=$(grep ' decomposed into' "$STDOUT" | awk -F' decomposed into' '{print $1}' | sort -u)
 echo -n 'Complexes: '
 echo "$COMPLEXES" | wc -l
 
 # Small molecules in complexes
 echo -n 'Complexes involving small molecules: '
-COMPLEXES_LINES=$(grep ' decomposed into' std.out | grep -v ' when removing small molecules' | sort -u)
-COMPLEXES_LINES2=$(grep ' decomposed into' std.out | grep ' when removing small molecules' | awk -F' when removing small molecules' '{print $1}' | sort -u)
+COMPLEXES_LINES=$(grep ' decomposed into' "$STDOUT" | grep -v ' when removing small molecules' | sort -u)
+COMPLEXES_LINES2=$(grep ' decomposed into' "$STDOUT" | grep ' when removing small molecules' | awk -F' when removing small molecules' '{print $1}' | sort -u)
 diff -U 0 <(echo "$COMPLEXES_LINES") <(echo "$COMPLEXES_LINES2") | grep -v '^@\|+++' | grep -c '^+'
 #diff -U 0 <(echo "$COMPLEXES_LINES") <(echo "$COMPLEXES_LINES2") | grep -v '^@\|+++' | grep '^+'
-
-echo ''
 
 # Iggy
 #cat iggy/result/stats.txt
